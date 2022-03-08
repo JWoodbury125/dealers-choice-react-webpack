10bc5eaeee3accdbc81085a0284732f8f010a13c
Jennifer Woodbury <jenann125@yahoo.com>
2022-03-08 17:50:29 -0500

    Heroku changes

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ const init = async () => {
 init();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
